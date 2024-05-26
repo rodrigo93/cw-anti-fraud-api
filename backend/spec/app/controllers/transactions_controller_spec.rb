1d4cabe3d4e7da36affa3206fd7b195964eafb37
Rodrigo Marques <rodrigomarques1993@gmail.com>
2024-05-26 17:36:52 -0300

    Add integration specs

--- backend/spec/app/controllers/transactions_controller_spec.rb
+++ backend/spec/app/controllers/transactions_controller_spec.rb
@@ -33,16 +33,59 @@ RSpec.describe TransactionsController, type: :controller do
     end
 
     context "when it's a fraudulent transaction" do
-      let(:transaction) { create :transaction, has_cbk: true }
+      let(:user_id) { 1234 }
 
-      it 'returns an approved response' do
-        subject
+      shared_examples 'denying the transaction' do
+        it do
+          subject
 
-        expect(response).to have_http_status :success
-        expect(parsed_body_response).to eq({
-                                             'transaction_id' => transaction.transaction_id,
-                                             'recommendation' => 'deny'
-                                           })
+          expect(response).to have_http_status :success
+          expect(parsed_body_response).to eq({
+                                               'transaction_id' => transaction.transaction_id,
+                                               'recommendation' => 'deny'
+                                             })
+        end
+      end
+
+      context 'when it does not comply with Rules::HasCbkRule' do
+        let(:transaction) { create :transaction, has_cbk: true }
+
+        it_behaves_like 'denying the transaction'
+      end
+
+      context 'when it does not comply with Rules::UnusualDeviceRule' do
+        before { create_list :transaction, 3, user_id: }
+
+        let(:transaction) { create :transaction, user_id:, device_id: 'unusual_device' }
+
+        it_behaves_like 'denying the transaction'
+      end
+
+      context 'when it does not comply with Rules::RepeatedCardNumberRule' do
+        before { create_list :transaction, 10, user_id:, card_number: transaction.card_number }
+
+        let(:transaction) { create :transaction, user_id: }
+
+        it_behaves_like 'denying the transaction'
+      end
+
+      context 'when it does not comply with Rules::TransactionAmountAndTimeRule' do
+        let(:transaction) do
+          create :transaction,
+                 user_id:,
+                 transaction_date: Time.zone.now.change(hour: 21),
+                 transaction_amount: Rules::TransactionAmountAndTimeRule::TRANSACTION_AMOUNT_LIMIT + 1
+        end
+
+        it_behaves_like 'denying the transaction'
+      end
+
+      context 'when it does not comply with Rules::MultipleTransactionsRule' do
+        before { create_list :transaction, Rules::MultipleTransactionsRule::TRANSACTION_LIMIT + 1, user_id: user_id }
+
+        let(:transaction) { create :transaction, user_id: user_id }
+
+        it_behaves_like 'denying the transaction'
       end
     end
 
