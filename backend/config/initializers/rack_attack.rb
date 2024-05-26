31ec564ec959e47c31a9d8c5ee3b6900f659da0b
Rodrigo Marques <rodrigomarques1993@gmail.com>
2024-05-26 15:16:52 -0300

    Add `rack-attack` to limit requests per client

---
+++ backend/config/initializers/rack_attack.rb
@@ -0,0 +1,5 @@
+class Rack::Attack
+  Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+
+  throttle('req/ip', limit: ENV.fetch('THROTTLE_LIMIT', 100).to_i, period: ENV.fetch('THROTTLE_PERIOD_SECONDS', 60).to_i.seconds, &:ip)
+end
