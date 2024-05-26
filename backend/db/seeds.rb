8fa8b51cf486170f1db42ea14761f37360d06c5c
Rodrigo Marques <rodrigomarques1993@gmail.com>
2024-05-26 12:32:49 -0300

    Add `Transaction` seed

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,8 +1,25 @@
 # frozen_string_literal: true
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+require 'csv'
+
+# Seed transactions
+current_transactions_amount = Transaction.count
+puts "Seeding transactions. Current transactions amount: #{current_transactions_amount}."
+
+CSV.foreach('spec/fixtures/transactional-sample.csv', headers: true) do |row|
+  Transaction.find_or_create_by(
+    transaction_id: row[0],
+    merchant_id: row[1],
+    user_id: row[2],
+    card_number: row[3],
+    transaction_date: row[4],
+    transaction_amount: row[5],
+    device_id: row[6],
+    has_cbk: row[7] == 'TRUE'
+  )
+end
+
+new_transactions_amount = Transaction.count - current_transactions_amount
+puts "Created #{new_transactions_amount} new transactions."
